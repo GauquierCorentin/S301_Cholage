13734a72d2bec2989264aed517a385a88096fdde
GauquierCorentin <corentin.gauquier@uphf.fr>
2023-09-07 18:38:58 +0200

    Debut page d'accueil avec logo + formulaire. Debut de structure du projet (MVC)

---
+++ PHP/Controler/Acceuil/Accueil.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+
+include("../../View/Accueil/Accueil.php");
+
+?>
---
+++ PHP/View/Accueil/Accueil.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Style/styleCholage.css">
+</head>
+
+<body>
+
+    <header>
+        <a class="logo" href="Accueil.php">
+            <img src="../Image/logo0.png" width=200 height=200 alt="">
+        </a>
+        <br><br>
+    </header>
+
+    <form method="post">
+        <div class="form-group">
+            <input type="email" class="form-control" placeholder="Email" name="mail" id="mail" aria-describedby="basic-addon1" required>
+        </div>
+
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Password" name="mdp" id="mdp" aria-describedby="basic-addon1">
+        </div>
+
+        <div>
+            <button type="submit" name="logSubmit" id="logSubmit" class="btn btn-info">Confirmer</button>
+        </div>
+    </form>
+    <a class="lien">Mot de passe oublié ?</a>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
