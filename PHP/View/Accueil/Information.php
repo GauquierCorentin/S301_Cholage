54b4de684090114963fa75e8925ff2ff34fd0e4b
matoww <matisse.gallouin@gmail.com>
2023-09-25 14:29:17 +0200

    Update Information.php
    Rajout de la présentation non vérifié par Mr polet

--- PHP/View/Accueil/Information.php
+++ PHP/View/Accueil/Information.php
@@ -15,9 +15,15 @@ include("../../View/BarreMenu/BarreMenu.php")
 ?>
 <div>
     <h1>Présentation</h1>
+    <p>Le chôlage est un sport de rue ressemblant à une sorte de golf, pratiqué dans quelques rares villages du nord dont notamment Quarouble ; les joueurs se réunissent une fois par an pour participer à un tournoi qui dure toute la journée qui est traditionnellement commencée par un petit déjeuner de tripes. Différent parcours sont préparés par le président du club et les perdants de chaque manche paient leur tournée au bar le plus proche du fût d’arrivée</p>
 </div>
 <div>
     <h1>Règles</h1>
+    <p>Les parcours et les équipes sont déterminées avant le commencement du tournoi, des ajustements peuvent être faits le jour même si nécessaire
+
+        Une partie de chôlage se déroule de la manière suivante :
+        Les deux équipes s’affrontant parient sur le nombre de chôles (c’est à dire de manches) qu’elles feront pour atteindre le fût de bière marquant l’arrivée, l’équipe annonçant le plus petit score sera l’équipe qui chôle tandis que l’autre sera l’équipe qui déchôle.
+        Une fois cela établi, les manches peuvent commencer, l’équipe chôlante joue trois coups à l’aide d’un maillet en direction du fût puis l’équipe déchôlante joue un coup dans la direction qu’elle veut, son but étant d’éloigner la balle du fût. Si l’équipe chôlante arrive à frapper le fût avec la chôlette (la balle) dans le nombre de chôles imparti, et si le score est validé par les deux capitaines, elle remporte la manche et c’est l’équipe déchôlante qui paie sa tournée et inversement dans le cas contraire. </p>
 </div>
 <div>
     <h1>Comment y participer ?</h1>
