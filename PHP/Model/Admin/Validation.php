7b9fbbfd5c330fe3947a0bc972e3757ed1bc0142
ElDiosAgua <anthony.williame@uphf.fr>
2023-10-06 09:57:42 +0200

    Update MVC

--- PHP/Model/Admin/Validation.php
+++ PHP/Model/Admin/Validation.php
@@ -10,39 +10,54 @@ try {
 } catch (PDOException $e) {
     die ('Erreur : ' . $e->getMessage());
 }
-//Requetes pour récupérer tout les users
-$requete = $pdo->prepare('SELECT * FROM users order by email');
-$requete->execute();
-$users = $requete->fetchAll(PDO::FETCH_ASSOC);
 
+function get_users($pdo)
+{
+//Requetes pour récupérer tout les users
+    $requete = $pdo->prepare('SELECT * FROM users order by email');
+    $requete->execute();
+    $users = $requete->fetchAll(PDO::FETCH_ASSOC);
+    return $users;
+}
+
+function get_dateValidation($pdo)
+{
+    $users = get_users($pdo);
 //On regarde la date de validation de chaque user
-foreach ($users as $item) {
-    $date = $item['datevalidation'];
-    $datephp = date("Y-m-d");
-    $dateValidUnAn = date("Y-m-d", strtotime($date . "+1 year"));
-    //Si la date de validation à un an de moins que la date du jour on update le users
-    if ($dateValidUnAn <= $datephp) {
-        $requete = $pdo->prepare('UPDATE users SET isvalidate = false WHERE email = ?');
-        $requete->execute(array($item['email']));
+    foreach ($users as $item) {
+        $date = $item['datevalidation'];
+        $datephp = date("Y-m-d");
+        $dateValidUnAn = date("Y-m-d", strtotime($date . "+1 year"));
+        //Si la date de validation à un an de moins que la date du jour on update le users
+        if ($dateValidUnAn <= $datephp) {
+            $requete = $pdo->prepare('UPDATE users SET isvalidate = false WHERE email = ?');
+            $requete->execute(array($item['email']));
+        }
     }
 }
 
+function get_usersNonValidate($pdo){
 //Request pour récupérer les users non validé
 
 $requete = $pdo->prepare('SELECT * FROM users WHERE isvalidate = false or datevalidation = null order by email');
 $requete->execute();
 $usersNonValidate = $requete->fetchAll(PDO::FETCH_ASSOC);
 
-//On modifie la valeur de isValidate à true
+return $usersNonValidate;
+}
 
-if(isset($_POST['submit'])){
-    $email = $_POST['test'];
-    echo ('On est dans la fonction pour valider un user');
-    $requete = $pdo->prepare('UPDATE users SET isvalidate = true WHERE email = ?');
-    $requete->execute(array($email));
+function feur($pdo)
+{
+//On modifie la valeur de isValidate à true et on ajoute la date de validation
+    if (isset($_POST['submit'])) {
+        $email = $_POST['test'];
+        echo('On est dans la fonction pour valider un user');
+        $requete = $pdo->prepare('UPDATE users SET isvalidate = true WHERE email = ?');
+        $requete->execute(array($email));
 
-    $date = date("Y-m-d");
-    $req = $pdo->prepare('UPDATE users SET datevalidation=? WHERE email = ?');
-    $req->execute(array($date,$email));
-    header('Location: ../../Controler/Admin/Validation.php');
+        $date = date("Y-m-d");
+        $req = $pdo->prepare('UPDATE users SET datevalidation=? WHERE email = ?');
+        $req->execute(array($date, $email));
+        header('Location: ../../View/Admin/Validation.php');
+    }
 }
--- PHP/View/Admin/Validation.php
+++ PHP/View/Admin/Validation.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+    require_once '../../View/BarreMenu/BarreMenu.php';
+    ?>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
           rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
@@ -13,6 +16,36 @@
 <input class=searchbar id="searchbar" onkeyup="search_Nom()" type="text"
        name="search" placeholder="recherche Nom">
 
+<?php
+require_once '../../Model/Admin/Validation.php';
+require_once '../../Controler/Admin/Validation.php';
+global $usersNonValidate;
+
+if ($usersNonValidate == null) {
+echo '<h1>Il n\'y a pas d\'utilisateur à valider</h1>';
+}
+
+//On affiche dans un tableau html tous les user Non Validé
+echo '<table class="tableau">';
+    echo '<tr>';
+        echo '<th>Mail</th>';
+        echo '<th>Nom</th>';
+        echo '<th>Prenom</th>';
+        echo '<th>Valider</th>';
+        echo '</tr>';
+    foreach ($usersNonValidate as $item) {
+    echo '<form method="post">';
+        echo '<tr class="test">';
+            echo '<td>' . $item['email'] . '</td>';
+            echo '<input type="hidden" name="test" value="' . $item['email'] . '">';
+            echo '<td class="nom">' . $item['nom'] . '</td>';
+            echo '<td>' . $item['prenom'] . '</td>';
+            echo '<td><input class="btn btn-info btn-sub-acc" type="submit" name="submit" value="Valider"></td>';
+            echo '</tr>';
+        echo '</form>';
+    }
+    echo('</table>');
+    ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
