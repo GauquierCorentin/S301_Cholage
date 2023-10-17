127567d5d1b1ad6fc20539d44ebda527a0233572
WeberGlh <guilhem.weber@uphf.fr>
2023-10-17 14:32:21 +0200

    reset password

---
+++ PHP/Controler/Password/ResetPassword.php
@@ -0,0 +1,41 @@
+<?php
+require('../../Model/Password/ResetPassword.php');
+require('../../View/Password/ResetPassword.php');
+
+require_once '../../Model/BDD/ConnexionBDD.php';
+//On récupère les données de la table User
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+try {
+    $conn = ConnexionBDD::getInstance();
+    $pdo = $conn::getpdo();
+} catch (PDOException $e) {
+    die ('Erreur : ' . $e->getMessage());
+}
+
+$mail = $_GET["email"];
+
+$pass1 = $_POST["pass1"];
+$pass2 = $_POST["pass2"];
+
+// Si mdp == null, on fait la vérification de token
+if ($pass1 == null) {
+    $creation = getCreationToken($mail);
+    $now = date("Y-m-d H:i:s");
+
+    if ($now('i') - $creation('i') <= 5) {
+        header('../../View/Password/ResetPassword.php');
+    } else {
+        $erreur = 'Délai de 5 minutes dépassé';
+        header('../../View/Password/RequestResetPassword.php?error_message=$erreur');
+    }
+} else { // sinon, l'utilisateur est dans le process donc on fait la modif
+    if ($pass1 != $pass2) { // si les mdp sont différents, on renvoie sur la page de changement
+        $erreur = 'Les mots de passe ne correspondent pas';
+        header('../../View/Password/ResetPassword.php?error_message=$erreur');
+    } else { // sinon on fait la modif
+        changePassword($mail, $pass1);
+    }
+}
---
+++ PHP/Model/Password/ResetPassword.php
@@ -0,0 +1,27 @@
+<?php
+require_once '../../Model/BDD/ConnexionBDD.php';
+//On récupère les données de la table User
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+try {
+    $conn = ConnexionBDD::getInstance();
+    $pdo = $conn::getpdo();
+} catch (PDOException $e) {
+    die ('Erreur : ' . $e->getMessage());
+}
+
+function getCreationToken($mail) {
+    global $pdo;
+    $date = $pdo->prepare('select creation from token where email = ?');
+    $date->execute(array($mail));
+    $getDate=$date->fetch();
+    return $getDate;
+}
+
+function changePassword($mail, $pass) {
+    global $pdo;
+    $change = $pdo->prepare('update users set password = ? where email = ?');
+    $change->execute(array($pass, $mail));
+
+}
