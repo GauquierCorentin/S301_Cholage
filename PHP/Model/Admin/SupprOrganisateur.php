e81f10e9c14d50e76845ff0a59445ba8241b86ff
ElDiosAgua <anthony.williame@uphf.fr>
2023-11-08 14:21:34 +0100

    Update SupprOrganisateur.php
    Ajout d'un envoie d'email

--- PHP/Model/Admin/SupprOrganisateur.php
+++ PHP/Model/Admin/SupprOrganisateur.php
@@ -1,5 +1,12 @@
 <?php
 require "../../Model/BDD/ConnexionBDD.php";
+require("../../Model/Includes/PHPMailer/src/Exception.php");
+require("../../Model/Includes/PHPMailer/src/PHPMailer.php");
+require ("../../Model/Includes/PHPMailer/src/SMTP.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 try {
     $conn = ConnexionBDD::getInstance();
     $pdo = $conn::getpdo();
@@ -17,6 +24,28 @@ function recupUsersOrga()
 function UpdateStatut($email){
     global $pdo;
     $requete=$pdo->prepare("Update users set isOrganisateur=false where email=?");
-    $requete->execute(array($email));
-    echo "vous avez modifié l'utilisateur ". $email;
+    $requete->execute(array($email));$mailer = new PHPMailer(true);
+    try {
+
+        //Server settings
+        $mailer->SMTPDebug = 0;
+        $mailer->isSMTP();
+        $mailer->Host = 'smtp.gmail.com';
+        $mailer->SMTPAuth = true;
+        $mailer->Username = 'cholage.offi@gmail.com';
+        $mailer->Password = 'fufvajtuygojmfro';
+        $mailer->SMTPSecure = 'tls';
+        $mailer->Port = 587;
+        //Recipients
+        $mailer->setFrom('cholage.offi@gmail.com', 'Cholage');
+        $mailer->Subject = 'Nouvel Organisateur';
+        //Remplacer le "S301_Cholage" par le nom du dossier qui contient le projet
+        $mailer->Body = 'Bonjour, nous avons le plaisir de vous annoncer que vous devenez un Organisateur';
+        $mailer->addAddress($email);
+        $mailer->send();
+        echo 'Message has been sent';
+        header('Location: ../../Controler/Admin/AjoutOrganisateur.php');
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mailer->ErrorInfo}";
+    }
 }
