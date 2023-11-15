68f38c200fc94634f6a6779b4a9f74bf95a2f7c1
ElDiosAgua <anthony.williame@uphf.fr>
2023-11-15 11:41:09 +0100

    Ajouter Questionnaire

---
+++ PHP/Controler/Tournoi/AjouterQuestionnaire.php
@@ -0,0 +1,4 @@
+<?php
+require_once '../../View/BarreMenu/BarreMenu.php';
+require_once ('../../Model/Tournoi/AjouterQuestionnaire.php');
+require_once ('../../View/Tournoi/AjouterQuestionnaire.php');
