c7ce3c4b90068f3630d72156bb29eba5539af800
WeberGlh <guilhem.weber@uphf.fr>
2023-11-17 15:26:54 +0100

    Début showQuestionnaire, pas fonctionnel

--- PHP/Model/Tournoi/ShowQuestionnaire.php
+++ PHP/Model/Tournoi/ShowQuestionnaire.php
@@ -1,4 +1,30 @@
 <?php
 require_once("../../Model/Tournoi/ShowQuestionnaire.php");
 require_once("../../View/Tournoi/ShowQuestionnaire.php");
-require_once ("../../View/BarreMenu/BarreMenu.php");
+require_once("../../View/BarreMenu/BarreMenu.php");
+require_once '../../Model/BDD/ConnexionBDD.php';
+
+
+try {
+    $conn = ConnexionBDD::getInstance();
+    $pdo = $conn::getpdo();
+} catch (PDOException $e) {
+    die ('Erreur : ' . $e->getMessage());
+}
+
+function getQuesitionnaires() {
+    global $pdo;
+    $req = $pdo->prepare('select * from questionnaire order by id');
+    $req->execute();
+    $rep = $req->fetchAll();
+    return $rep;
+}
+
+function getQuestions(){
+    global $pdo;
+    $req = $pdo->prepare('select * from question join public.reponse r on question.idquestion = r.idquestion order by idquestionnaire');
+    $req->execute();
+    $rep = $req->fetchAll();
+    return $rep;
+}
+
--- PHP/View/Tournoi/ShowQuestionnaire.php
+++ PHP/View/Tournoi/ShowQuestionnaire.php
@@ -1,1 +1,36 @@
 <?php
+$questionnaires = $_SESSION['showQuestionnaires'];
+$questions = $_SESSION['showQuestions'];
+
+?>
+<script src="../../Model/Fonctions/functions.js"></script>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Répondre aux questionnaires</title>
+    <link rel="stylesheet" href="../../CSS/Style.css">
+</head>
+
+<body>
+<form method="POST">
+    <?php
+
+    foreach ($questionnaires as $i) {
+        echo('<div>');
+        echo($i[1] . "<br>");
+        foreach ($questions as $q) {
+            if ($q[2] == $i[0]) {
+                echo($q[1] . "<br>");
+                foreach ($q as $r) {
+                    echo($q[4] . "<br>");
+                }
+            }
+        }
+    }
+    echo("<br>");
+    echo('</div>');
+
+    ?>
+</form>
+</body>
