b1318ae17a929425a1a566c81bd379869c42d225
ElDiosAgua <anthony.williame@uphf.fr>
2023-11-24 15:34:51 +0100

    Update afficherEquipe.php

--- PHP/Controller/Tournoi/afficherEquipe.php
+++ PHP/Controller/Tournoi/afficherEquipe.php
@@ -1,9 +1,10 @@
 <?php
 include("../../View/BarreMenu/BarreMenu.php");
-include("../../View/Tournoi/afficherEquipe.php");
 include("../../Model/Tournoi/afficherEquipe.php");
+
 
 $equipe = getEquipe();
 $_SESSION['equipe'] = $equipe;
 $membreEquipe = getMembreEquipe();
 $_SESSION['mbEquipe'] = $membreEquipe;
+include("../../View/Tournoi/afficherEquipe.php");
