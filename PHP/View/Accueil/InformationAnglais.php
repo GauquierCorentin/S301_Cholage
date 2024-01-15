9d07fd298799c45efb9c7e5324cb19ebf49dba08
WeberGlh <guilhem.weber@uphf.fr>
2024-01-15 08:39:49 +0100

    ajout informations en anglais, faut faire les liens et tout

---
+++ PHP/View/Accueil/InformationAnglais.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+include("../../View/BarreMenu/BarreMenu.php")
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informations</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Style/styleInformation.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+            crossorigin="anonymous"></script>
+</head>
+
+<body>
+<div>
+    <h1>Presentation</h1>
+    <p>"Le Chôlage" is a street sport, resembling golf, played in a small number of villages in the north of France,
+        notably Quarouble ; Once a year, the players organize a tournament which is traditionally started by a tripe
+        breakfast. A number of courses are prepared by the club president and each round's losers pay their at the pub
+        closest to the finish line, which is a beer keg.
+    </p>
+</div>
+<div>
+    <h1>Presentation Video</h1>
+    <div class="Video">
+        <iframe src="https://www.dailymotion.com/embed/video/x88gicg" allowfullscreen
+                title="Dailymotion Video Player"></iframe>
+    </div>
+</div>
+<div>
+    <h1>Rules</h1>
+    <p>Courses and teams are determined before the start of the tournaments, if necessary, adjustments can be made on
+        the day of the tournament.
+
+        A game of "Chôlage" is played as follows
+        Both teams bet on the number of "chôles" (ie: sets) they are going to play to reach the keg marking the finish
+        line, the team who bets the smallest number will be the "chôle" team while the other will be the "déchôle" team.
+        Once this is set, the matches may begin, the "chôle" team plays three shots with a large mallet towards the keg,
+        then the "déchôle" team plays one shot in whichever direction they want, aiming to drive the ball away.
+        If the "chôle" team manages to hit the keg in a number of "chôles" smaller or equal to the number they bet,
+        and if the score has been confirmed by both captain, they win the round, and it is the "déchôle" team who pays
+        their round and vice versa. </p>
+</div>
+<div>
+    <h1>Participate</h1>
+</div>
+<div>
+    <h1>Useful Information</h1>
+    <h2>Contact</h2>
+    <h2>Place</h2>
+</div>
+</body>
+</html>
+
